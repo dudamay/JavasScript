9037538f553dae7468addc17f0b51df31c76bb07
dudamay <maysaeduarda1006@gmail.com>
2024-05-14 08:37:43 -0400

    adicionado exemplo DadosPrimitivos.js

---
+++ DadosPrimitivos.js
@@ -0,0 +1,13 @@
+let nome = "Wanderson";
+let verdadeiro = true;
+let falso = false;
+let numero = 40;
+let reais = 25.99;
+let nulo = null;
+let NotANumber = 0 / 0;
+let objeto = {
+    name: "Wanderson",
+    age: 40,
+    Profession: "Teacher"
+    
+}
