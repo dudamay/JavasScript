e215bb56401d9fff869335ad7a09c6cb0523f5c2
dudamay <maysaeduarda1006@gmail.com>
2024-05-14 08:39:34 -0400

    adicionado exemplo Template-Literals.js

---
+++ Template-Literals.js
@@ -0,0 +1,6 @@
+let nome = " Wanderson Timóteo";
+let idade = 30;
+
+// Usando template literals para criar uam string com variáveis
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); // Saída:" Olá, meu nome é Wanderson Timóteo e eu tenho 30 anos."
