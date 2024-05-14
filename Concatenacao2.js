b6f3677a446587d1a00724fbfd40568a4093cbf9
dudamay <maysaeduarda1006@gmail.com>
2024-05-14 08:33:31 -0400

    adicionado exemplo concatenacao 2

---
+++ Concatenacao2.js
@@ -0,0 +1,4 @@
+let nome = "Felipe";
+let idade = 8;
+let saudacao = "Olá, meu nome é" + nome + "e eu tenho" + idade + "anos.";
+console.log(saudacao);// Saída: "Olá, meu nome é Felipe e eu teno 08 anos."
