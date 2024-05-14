dc32b09ff29119cbd50027abab26df1fd31e2716
dudamay <maysaeduarda1006@gmail.com>
2024-05-14 11:12:54 -0400

    exemplo template

---
+++ Template-literals3.js
@@ -0,0 +1,15 @@
+let textoMultimidia = `
+Esta é uma string
+que ocupa múltiplas linhas
+e você não precisa se preocupar
+com quebras de linha ou concatenação.
+`;
+console.log(textoMultimidia);
+/*
+Saída:
+" 
+Esta é uma string
+que ocupa múltiplas linhas
+e você não precisa se preocupar com quebras de linha ou concatenação.
+ "
+ */
